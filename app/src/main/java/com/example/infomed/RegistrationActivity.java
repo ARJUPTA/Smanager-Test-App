a3d2c92cc047f2a3b304812fbb082a18c26527e4
bhoomik bhamawat <bhoomikbhamawat.eee18@itbhu.ac.in>
2019-03-06 20:10:26 +0530

    login change

--- app/src/main/java/com/example/infomed/RegistrationActivity.java
+++ app/src/main/java/com/example/infomed/RegistrationActivity.java
@@ -2,12 +2,41 @@ package com.example.infomed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
+    private EditText name,number,email,pwd,cfmpwd;
+    private Button register;
+    private FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+
+
+        name=findViewById(R.id.input_name);
+        number=findViewById(R.id.input_phone);
+        email=findViewById(R.id.input_email);
+        pwd=findViewById(R.id.input_password);
+        cfmpwd=findViewById(R.id.input_confirm_password);
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+
     }
 }
